507c240fadbcb97d7ac8602d0b3eb3ff4be1d57c
Divya-Bodake <divyabodake25@gmail.com>
2022-11-21 01:01:11 +0530

    [Divya]Add:Perfect Number

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -4,8 +4,31 @@
     {
         static void Main(string[] args)
         {
-            FibonacciSeries fibonacciSeries = new FibonacciSeries();
-            fibonacciSeries.CalculateFibonacciSeries();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select:-1.FibonacciSeries \n 2.PerfectNumber");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                        fibonacciSeries.CalculateFibonacciSeries();
+                        break;
+                        case 2:
+                        PerfectNumber perfectNum = new PerfectNumber();
+                        perfectNum.CheckPerfectNumber();
+                        break;
+
+                }
+                
+            }
+                
+
+
+
+
+               
         }
     }
 }
