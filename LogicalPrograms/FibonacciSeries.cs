bde3fd1f64d57292165d582ed867453fd6267839
Divya-Bodake <divyabodake25@gmail.com>
2022-11-21 00:29:56 +0530

    [Divya]Add:Fibonacci Series

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        static int num1 = 0, num2 = 1, num3, num;
+        public void CalculateFibonacciSeries()
+        {
+            Console.WriteLine("Enter the number of ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(num1 + " " + num2 + " ");
+            for (int i = 2; i < num; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
+
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciSeries fibonacciSeries = new FibonacciSeries();
+            fibonacciSeries.CalculateFibonacciSeries();
+        }
+    }
+}
